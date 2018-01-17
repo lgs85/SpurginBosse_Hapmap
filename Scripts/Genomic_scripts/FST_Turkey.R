2f9698c8e06ba7d6ab6fb56872ea6a200c8a6671
lgs85 <lewisspurgin@gmail.com>
2018-01-17 17:29:32 +0000

    Edits

---
+++ Scripts/Genomic_scripts/FST_Turkey.R
@@ -0,0 +1,105 @@
+rm(list=ls())
+
+pd <- read.table("../../Data/PairwiseFST.txt",header = T,stringsAsFactors = F)
+recomb <- read.table("../../Data/500kb_recombination.txt",header = T)
+
+
+pd <- subset(pd,pop1 == "Turkey"|pop2 == "Turkey")
+
+for(i in 1:nrow(pd))
+{
+  if(pd$pop1[i] == "Turkey")
+  {
+    pd$pop1[i] <- pd$pop2[i]
+    pd$pop2[i] <- "Turkey"
+  }
+}
+
+pops <- unique(unlist(pd[order(pd$FST),"pop1"]))
+
+
+pops <- pops[!(pops %in% c("Turkey","Antwerp_Belgium","Harjavalta_Finland","La_Rouviere_France",
+                           "Font_Roja_Spain","Cambridge_UK","Radolfzell_Germany","Bulgaria","Romania",
+                           "Vienna_Austria","Tartu_Estonia","Westerheide","Zurich_Switzerland","Velky_Kosir_Czech_Republic"))]
+
+
+
+
+for (i in 1:length(pops))
+{
+  system(paste0("vcftools --vcf ~/Documents/Research/Great_tit_hapmap/vcftools/HapMapMajor.vcf --out temp --fst-window-size 500000 --fst-window-step 500000 --weir-fst-pop ~/Documents/Research/Great_tit_hapmap/vcftools/popfiles/Turkey.txt --weir-fst-pop ~/Documents/Research/Great_tit_hapmap/vcftools/popfiles/", pops[i], ".txt"))
+  temp <- read.table("temp.windowed.weir.fst",header = T,stringsAsFactors = F)
+  temp$Pop <- pops[i]
+  
+  if(i == 1)
+  {
+    write.table(temp,"temp.fst",row.names = F,col.names = T,quote = F)
+    system("cp temp.fst Turkey.fst")
+  } else
+  {
+    write.table(temp,"temp.fst",row.names = F,col.names = F, quote = F)
+    system("cat Turkey.fst temp.fst > temp.file")
+    system("mv temp.file Turkey.fst")
+  }
+}
+
+
+
+
+
+dd <- read.table("Turkey.fst",header = T,stringsAsFactors = F)
+
+
+recomb$BIN_START <- recomb$POS500KB + 1
+recomb$CHROM <- recomb$V3
+recomb$V3 <- NULL
+
+inall <- Reduce(intersect, list(paste(recomb$CHROM,recomb$BIN_START),
+                                paste(dd$CHROM,dd$BIN_START)))
+
+dd <- subset(dd,paste(dd$CHROM,dd$BIN_START) %in% inall)
+dd$r <- NA
+
+for(i in 1:nrow(recomb))
+{
+  dd$r[paste(dd$CHROM,dd$BIN_START) == paste(recomb$CHROM[i],recomb$BIN_START[i])] <- recomb$MEAN_cM[i]
+}
+
+
+
+q <- quantile(dd$r,0.05)
+dd$rf <- ifelse(dd$r < q,"Low","Not low")
+
+dd$Pop <- factor(dd$Pop,levels = unique(dd$Pop))
+
+dd <- subset(dd,CHROM < 36)
+dd$MEAN_FST[dd$MEAN_FST < 0] <- 0
+library(ggplot2)
+
+dd$x <- NA
+pops <- unique(dd$Pop)
+for(i in 1:length(pops))
+{
+  dd$x[dd$Pop == pops[i]] <- 1:sum(dd$Pop == pops[i])
+}
+
+
+Fig4A <- ggplot(dd,aes(x = x,y = MEAN_FST,col = rf))+
+  geom_line(aes(group=1))+
+  theme_classic()+
+  theme(
+    strip.background = element_blank(),
+    #strip.placement = "outside",
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank())+
+  xlab("")+
+  ylab(expression(italic(F)[ST])) + 
+  facet_wrap(~Pop,ncol = 2) +
+  theme(axis.line.y = element_line(),
+        axis.line.x = element_line(),
+        strip.text.x = element_text(size = 6),
+        legend.position = "none")+
+  scale_colour_manual(values = c("navy","grey"))
+
+Fig4A
+
