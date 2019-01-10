88565393ab042f6a32dc3b097da395837323e42d
lgs85 <lewisspurgin@gmail.com>
2019-01-10 20:18:17 +0000

    tidyversing load script

--- Scripts/SpurginBosse_Hapmap_LOAD.R
+++ Scripts/SpurginBosse_Hapmap_LOAD.R
@@ -1,51 +1,55 @@
-library(ggplot2)
+library(tidyverse)
 library(ggmap)
 library(rworldmap)
-library(magrittr)
-library(reshape)
 library(Rmisc)
 library(geosphere)
 library(ecodist)
 library(kableExtra)
 
-ll <- read.table("Data/LatLongAllPops.txt",header = F,stringsAsFactors = F)
+
+ll <- read_tsv("Data/LatLongAllPops.txt",col_names = F)
 
 
 for(i in 2:10)
 {
-  assign(paste0("admix",i),read.table(paste0("Data/HapMapMajorPruned.", i, ".Q")))
+  assign(str_c("admix",i),read_delim(str_c("Data/HapMapMajorPruned.", i, ".Q"),delim = " ",col_names = F))
 }
 
 
 
 
-pops <- read.table("Data/HapMapMajor.fam",stringsAsFactors = F)
-p_markers <- read.table("Data/HapMapMajorPruned.bim",stringsAsFactors = F)
-markers <- read.table("Data/HapMapMajor.bim",stringsAsFactors = F)
-pd <- read.table("Data/PairwiseFST.txt",stringsAsFactors = F,header = T)
-het <- read.table("Data/PopHet.txt",header = T)
-ldmean <- read.table("Data/LD.txt",header = F)
-ld <- read.table("Data/HapMapLD.txt",header = F,stringsAsFactors = F)
-cv <- read.table("Data/CV_error.txt",header = T)
-recomb <- read.table("Data/recomb_kai_500kb.txt",header = T,stringsAsFactors = F)
-tu <- read.table("Data/Turkey.fst",header = T,stringsAsFactors = F)
-outlierhaps <- read.table("Data/Outliers_for_Mirte_HAP_statistics1.txt",header = T)
-pca <- read.table("Data/HapMap.eigenvec", stringsAsFactors = F)
+pops <- read_delim("Data/HapMapMajor.fam", delim = " ",col_names = F)
+p_markers <- read_tsv("Data/HapMapMajorPruned.bim", col_names = F)
+markers <- read_tsv("Data/HapMapMajor.bim", col_names = F)
+pd <- read_tsv("Data/PairwiseFST.txt")
+het <- read_tsv("Data/PopHet.txt")
+ldmean <- read_tsv("Data/LD.txt", col_names = F)
+ld <- read_delim("Data/HapMapLD.txt", delim = " ", col_names = F)
+cv <- read_tsv("Data/CV_error.txt")
+recomb <- read_delim("Data/recomb_kai_500kb.txt", delim = " ")
+tu <- read_delim("Data/Turkey.fst", delim = " ")
+outlierhaps <- read_tsv("Data/Outliers_for_Mirte_HAP_statistics1.txt")
+pca <- read_delim("Data/HapMap.eigenvec", delim = " ", col_names = F)
 
 
 #Windowed stats
 fn <- list.files("Data/Windowed_stats")
-popname <- substr(fn,1,nchar(fn)-7)
+popname <- str_sub(fn,1,str_length(fn)-7)
 
 for(i in c(1:length(fn)))
 {
-  cd <-  read.csv(gzfile(paste0("Data/Windowed_stats/",popname[i],".csv.gz")))
-  colnames(cd)[6:9] <- c("pi_pop1","pi_Turkey","dxy","FST")
-  cd$pop1 <- rep(popname[i],nrow(cd))
-  cd$zFST <- (cd$FST - mean(cd$FST))/sd(cd$FST)
-  cd$zdxy <- (cd$dxy - mean(cd$dxy))/sd(cd$dxy)
-  cd$zpi <- (cd$pi_pop1 - mean(cd$pi_pop1))/sd(cd$pi_pop1)
-  cd$order <- c(1:nrow(cd))
+  cd <-  read_csv(gzfile(str_c("Data/Windowed_stats/",popname[i],".csv.gz")))
+  cd <- cd %>% 
+    dplyr::rename(pi_pop1 = 6,
+                       pi_Turkey = 7,
+                       dxy = 8,
+                       FST = 9) %>%
+    mutate(pop1 = popname[i],
+           zFST = (FST - mean(FST))/sd(FST),
+           zdxy = (dxy - mean(dxy))/sd(dxy),
+           zpi = (pi_pop1 - mean(pi_pop1))/sd(pi_pop1),
+           order = c(1:nrow(cd)))
+
   if(i == 1) dw <- cd else dw <- rbind(dw,cd)
 }
 
